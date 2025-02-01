e6ce4eca35ef2df3fabba4ce7d147c7e7be8cb5d
wahib bachoua <wahibbachoua95@gmail.com>
2025-02-01 16:32:05 +0100

    first commit

---
+++ MyNewWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyNewWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
